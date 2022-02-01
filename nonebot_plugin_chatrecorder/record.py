b9f2f895d1bcb6465f90763bb0f56e8a1988f1d5
meetwq <meetwq@gmail.com>
2022-02-01 17:58:42 +0800

    fix typing

--- nonebot_plugin_chatrecorder/record.py
+++ nonebot_plugin_chatrecorder/record.py
@@ -1,5 +1,6 @@
+from datetime import datetime
 from sqlmodel import select, or_
-from typing import Iterable, List, Optional, Literal, Union
+from typing import Iterable, List, Optional, Literal, Union, overload
 
 from nonebot.adapters.onebot.v11 import Message
 from nonebot_plugin_datastore import create_session
@@ -8,15 +9,43 @@ from .model import MessageRecord
 from .message import deserialize_message
 
 
+@overload
 async def get_message_records(
+    plain_text: Literal[True],
     user_ids: Optional[Iterable[str]] = None,
     group_ids: Optional[Iterable[str]] = None,
     exclude_user_ids: Optional[Iterable[str]] = None,
     exclude_group_ids: Optional[Iterable[str]] = None,
     message_type: Optional[Literal['private', 'group']] = None,
-    time_start: Optional[int] = None,
-    time_stop: Optional[int] = None,
+    time_start: Optional[datetime] = None,
+    time_stop: Optional[datetime] = None,
+) -> List[str]:
+    ...
+
+
+@overload
+async def get_message_records(
+    plain_text: Literal[False] = ...,
+    user_ids: Optional[Iterable[str]] = None,
+    group_ids: Optional[Iterable[str]] = None,
+    exclude_user_ids: Optional[Iterable[str]] = None,
+    exclude_group_ids: Optional[Iterable[str]] = None,
+    message_type: Optional[Literal['private', 'group']] = None,
+    time_start: Optional[datetime] = None,
+    time_stop: Optional[datetime] = None,
+) -> List[Message]:
+    ...
+
+
+async def get_message_records(
     plain_text: bool = False,
+    user_ids: Optional[Iterable[str]] = None,
+    group_ids: Optional[Iterable[str]] = None,
+    exclude_user_ids: Optional[Iterable[str]] = None,
+    exclude_group_ids: Optional[Iterable[str]] = None,
+    message_type: Optional[Literal['private', 'group']] = None,
+    time_start: Optional[datetime] = None,
+    time_stop: Optional[datetime] = None,
 ) -> Union[List[str], List[Message]]:
     """
     :说明:
@@ -25,18 +54,18 @@ async def get_message_records(
 
     :参数:
 
+      * ``plain_text: bool = False``: 为真则返回字符串数组，否则返回 Message 数组
       * ``user_ids: Optional[Iterable[str]]``: 用户列表，为空表示所有用户
       * ``group_ids: Optional[Iterable[str]]``: 群组列表，为空表示所有群组
       * ``exclude_user_ids: Optional[Iterable[str]]``: 不包含的用户列表，为空表示不限制
       * ``exclude_group_ids: Optional[Iterable[str]]``: 不包含的群组列表，为空表示不限制
       * ``message_type: Optional[Literal['private', 'group']]``: 消息类型，可选值：'private' 和 'group'，为空表示所有类型
-      * ``time_start: Optional[int]``: 起始时间，类型为时间戳，单位为秒，为空表示不限制起始时间
-      * ``time_stop: Optional[int]``: 结束时间，类型为时间戳，单位为秒，为空表示不限制结束时间
-      * ``plain_text: bool = False``: 为真则返回字符串数组，否则返回 Message 数组
+      * ``time_start: Optional[datetime]``: 起始时间，UTC 时间，为空表示不限制起始时间
+      * ``time_stop: Optional[datetime]``: 结束时间，UTC 时间，为空表示不限制结束时间
 
     :返回值:
 
-      * ``List[Message]``: 消息列表
+      * ``Union[List[str], List[Message]]``: 消息列表
     """
 
     whereclause = []
