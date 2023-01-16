9c1a09a83e92c501c176ac7037378dee1a7e5432
meetwq <meetwq@gmail.com>
2023-01-16 16:39:11 +0800

    适配 Onebot V12

--- nonebot_plugin_chatrecorder/__init__.py
+++ nonebot_plugin_chatrecorder/__init__.py
@@ -3,31 +3,44 @@ from typing import Any, Dict, Optional
 
 from nonebot import get_driver, require
 from nonebot.adapters import Bot as BaseBot
-from nonebot.adapters.onebot.v11 import Bot, GroupMessageEvent, Message, MessageEvent
 from nonebot.message import event_postprocessor
+
+from nonebot.adapters.onebot.v11 import Bot as V11Bot
+from nonebot.adapters.onebot.v11 import Message as V11Msg
+from nonebot.adapters.onebot.v11 import MessageEvent as V11MEvent
+from nonebot.adapters.onebot.v11 import GroupMessageEvent as V11GMEvent
+
+from nonebot.adapters.onebot.v12 import Bot as V12Bot
+from nonebot.adapters.onebot.v12 import Message as V12Msg
+from nonebot.adapters.onebot.v12 import MessageEvent as V12MEvent
+from nonebot.adapters.onebot.v12 import GroupMessageEvent as V12GMEvent
+from nonebot.adapters.onebot.v12 import ChannelMessageEvent as V12CMEvent
 
 require("nonebot_plugin_datastore")
 from nonebot_plugin_datastore import create_session
 
 from .config import Config
-from .message import serialize_message
 from .model import MessageRecord
-from .record import get_message_records as get_message_records
+from .message import serialize_message
+from .record import get_message_records, get_messages
+
+plugin_config = Config.parse_obj(get_driver().config.dict())
 
 
 @event_postprocessor
-async def record_recv_msg(event: MessageEvent):
-
+async def _(bot: V11Bot, event: V11MEvent):
     record = MessageRecord(
+        bot_type=bot.type,
+        bot_id=bot.self_id,
         platform="qq",
         time=datetime.utcfromtimestamp(event.time),
-        type="message",
-        detail_type="group" if isinstance(event, GroupMessageEvent) else "private",
+        type=event.post_type,
+        detail_type=event.message_type,
         message_id=str(event.message_id),
         message=serialize_message(event.message),
         alt_message=event.message.extract_plain_text(),
         user_id=str(event.user_id),
-        group_id=str(event.group_id) if isinstance(event, GroupMessageEvent) else "",
+        group_id=str(event.group_id) if isinstance(event, V11GMEvent) else "",
     )
 
     async with create_session() as session:
@@ -35,33 +48,22 @@ async def record_recv_msg(event: MessageEvent):
         await session.commit()
 
 
-async def record_send_msg(
-    bot: BaseBot,
-    e: Optional[Exception],
-    api: str,
-    data: Dict[str, Any],
-    result: Optional[Dict[str, Any]],
-):
-
-    if e or not result:
-        return
-    if api not in ["send_msg", "send_private_msg", "send_group_msg"]:
-        return
-
-    message = Message(data["message"])
+@event_postprocessor
+async def _(bot: V12Bot, event: V12MEvent):
     record = MessageRecord(
-        platform="qq",
-        time=datetime.utcnow(),
-        type="message",
-        detail_type="group"
-        if api == "send_group_msg"
-        or (api == "send_msg" and data["message_type"] == "group")
-        else "private",
-        message_id=str(result["message_id"]),
-        message=serialize_message(message),
-        alt_message=message.extract_plain_text(),
-        user_id=str(bot.self_id),
-        group_id=str(data.get("group_id", "")),
+        bot_type=bot.type,
+        bot_id=bot.self_id,
+        platform=event.self.platform,
+        time=event.time,
+        type=event.type,
+        detail_type=event.detail_type,
+        message_id=event.message_id,
+        message=serialize_message(event.message),
+        alt_message=event.alt_message,
+        user_id=event.user_id,
+        group_id=event.group_id if isinstance(event, V12GMEvent) else "",
+        guild_id=event.guild_id if isinstance(event, V12CMEvent) else "",
+        channel_id=event.channel_id if isinstance(event, V12CMEvent) else "",
     )
 
     async with create_session() as session:
@@ -69,6 +71,74 @@ async def record_send_msg(
         await session.commit()
 
 
-plugin_config = Config.parse_obj(get_driver().config.dict())
 if plugin_config.chatrecorder_record_send_msg:
-    Bot.on_called_api(record_send_msg)
+
+    @V11Bot.on_called_api
+    async def _(
+        bot: BaseBot,
+        e: Optional[Exception],
+        api: str,
+        data: Dict[str, Any],
+        result: Optional[Dict[str, Any]],
+    ):
+        if e or not result:
+            return
+        if api not in ["send_msg", "send_private_msg", "send_group_msg"]:
+            return
+
+        message = V11Msg(data["message"])
+        record = MessageRecord(
+            bot_type=bot.type,
+            bot_id=bot.self_id,
+            platform="qq",
+            time=datetime.utcnow(),
+            type="message_sent",
+            detail_type="group"
+            if api == "send_group_msg"
+            or (api == "send_msg" and data["message_type"] == "group")
+            else "private",
+            message_id=str(result["message_id"]),
+            message=serialize_message(message),
+            alt_message=message.extract_plain_text(),
+            user_id=str(bot.self_id),
+            group_id=str(data.get("group_id", "")),
+        )
+
+        async with create_session() as session:
+            session.add(record)
+            await session.commit()
+
+    @V12Bot.on_called_api
+    async def _(
+        bot: BaseBot,
+        e: Optional[Exception],
+        api: str,
+        data: Dict[str, Any],
+        result: Optional[Dict[str, Any]],
+    ):
+        if e or not result:
+            return
+        if api not in ["send_message"]:
+            return
+        assert isinstance(bot, V12Bot)
+
+        message = V12Msg(data["message"])
+        record = MessageRecord(
+            bot_type=bot.type,
+            bot_id=bot.self_id,
+            platform=bot.platform,
+            time=datetime.utcfromtimestamp(result["time"]),
+            type="message_sent",
+            detail_type=data["detail_type"],
+            message_id=result["message_id"],
+            message=serialize_message(message),
+            alt_message=message.extract_plain_text(),
+            user_id=str(bot.self_id),
+            group_id=data.get("group_id", ""),
+            guild_id=data.get("guild_id", ""),
+            channel_id=data.get("channel_id", ""),
+        )
+
+        async with create_session() as session:
+            session.add(record)
+            await session.commit()
--- nonebot_plugin_chatrecorder/record.py
+++ nonebot_plugin_chatrecorder/record.py
@@ -1,52 +1,35 @@
 from datetime import datetime
+from sqlmodel import or_, select
 from typing import Iterable, List, Literal, Optional, Union, overload
 
-from nonebot.adapters.onebot.v11 import Message
+from nonebot.adapters.onebot.v11 import Bot as V11Bot
+from nonebot.adapters.onebot.v11 import Message as V11Msg
+
+from nonebot.adapters.onebot.v12 import Bot as V12Bot
+from nonebot.adapters.onebot.v12 import Message as V12Msg
+
 from nonebot_plugin_datastore import create_session
-from sqlmodel import or_, select
 
-from .message import deserialize_message
 from .model import MessageRecord
-
-
-@overload
-async def get_message_records(
-    plain_text: Literal[True],
-    user_ids: Optional[Iterable[str]] = None,
-    group_ids: Optional[Iterable[str]] = None,
-    exclude_user_ids: Optional[Iterable[str]] = None,
-    exclude_group_ids: Optional[Iterable[str]] = None,
-    message_type: Optional[Literal["private", "group"]] = None,
-    time_start: Optional[datetime] = None,
-    time_stop: Optional[datetime] = None,
-) -> List[str]:
-    ...
-
-
-@overload
-async def get_message_records(
-    plain_text: Literal[False] = ...,
-    user_ids: Optional[Iterable[str]] = None,
-    group_ids: Optional[Iterable[str]] = None,
-    exclude_user_ids: Optional[Iterable[str]] = None,
-    exclude_group_ids: Optional[Iterable[str]] = None,
-    message_type: Optional[Literal["private", "group"]] = None,
-    time_start: Optional[datetime] = None,
-    time_stop: Optional[datetime] = None,
-) -> List[Message]:
-    ...
+from .message import deserialize_message
 
 
 async def get_message_records(
-    plain_text: bool = False,
+    bot: Union[V11Bot, V12Bot],
+    *,
     user_ids: Optional[Iterable[str]] = None,
     group_ids: Optional[Iterable[str]] = None,
+    guild_ids: Optional[Iterable[str]] = None,
+    channel_ids: Optional[Iterable[str]] = None,
     exclude_user_ids: Optional[Iterable[str]] = None,
     exclude_group_ids: Optional[Iterable[str]] = None,
-    message_type: Optional[Literal["private", "group"]] = None,
+    exclude_guild_ids: Optional[Iterable[str]] = None,
+    exclude_channel_ids: Optional[Iterable[str]] = None,
+    types: Optional[Iterable[Literal["message", "message_sent"]]] = None,
+    detail_types: Optional[Iterable[str]] = None,
     time_start: Optional[datetime] = None,
     time_stop: Optional[datetime] = None,
-) -> Union[List[str], List[Message]]:
+) -> List[MessageRecord]:
     """
     :说明:
 
@@ -54,21 +37,27 @@ async def get_message_records(
 
     :参数:
 
-      * ``plain_text: bool = False``: 为真则返回字符串数组，否则返回 Message 数组
+      * ``bot: Union[V11Bot, V12Bot]``: Nonebot `Bot` 对象
       * ``user_ids: Optional[Iterable[str]]``: 用户列表，为空表示所有用户
       * ``group_ids: Optional[Iterable[str]]``: 群组列表，为空表示所有群组
+      * ``guild_ids: Optional[Iterable[str]]``: 两级群组消息群组列表，为空表示所有群组
+      * ``channel_ids: Optional[Iterable[str]]``: 两级群组消息频道列表，为空表示所有频道
       * ``exclude_user_ids: Optional[Iterable[str]]``: 不包含的用户列表，为空表示不限制
       * ``exclude_group_ids: Optional[Iterable[str]]``: 不包含的群组列表，为空表示不限制
-      * ``message_type: Optional[Literal['private', 'group']]``: 消息类型，可选值：'private' 和 'group'，为空表示所有类型
+      * ``exclude_guild_ids: Optional[Iterable[str]]``: 不包含的两级群组消息群组列表，为空表示不限制
+      * ``exclude_channel_ids: Optional[Iterable[str]]``: 不包含的两级群组消息频道列表，为空表示不限制
+      * ``types: Optional[Iterable[Literal["message", "message_sent"]]]``: 消息事件类型列表，为空表示所有类型
+      * ``detail_types: Optional[List[str]]``: 消息事件具体类型列表，为空表示所有类型
       * ``time_start: Optional[datetime]``: 起始时间，UTC 时间，为空表示不限制起始时间
       * ``time_stop: Optional[datetime]``: 结束时间，UTC 时间，为空表示不限制结束时间
 
     :返回值:
 
-      * ``Union[List[str], List[Message]]``: 消息列表
+      * ``List[MessageRecord]``: 消息记录列表
     """
 
     whereclause = []
+    whereclause.append(MessageRecord.bot_id == bot.self_id)
     if user_ids:
         whereclause.append(
             or_(*[MessageRecord.user_id == user_id for user_id in user_ids])  # type: ignore
@@ -77,14 +66,32 @@ async def get_message_records(
         whereclause.append(
             or_(*[MessageRecord.group_id == group_id for group_id in group_ids])  # type: ignore
         )
+    if guild_ids:
+        whereclause.append(
+            or_(*[MessageRecord.guild_id == guild_id for guild_id in guild_ids])  # type: ignore
+        )
+    if channel_ids:
+        whereclause.append(
+            or_(*[MessageRecord.channel_id == channel_id for channel_id in channel_ids])  # type: ignore
+        )
     if exclude_user_ids:
         for user_id in exclude_user_ids:
             whereclause.append(MessageRecord.user_id != user_id)
     if exclude_group_ids:
         for group_id in exclude_group_ids:
             whereclause.append(MessageRecord.group_id != group_id)
-    if message_type:
-        whereclause.append(MessageRecord.detail_type == message_type)
+    if exclude_guild_ids:
+        for guild_id in exclude_guild_ids:
+            whereclause.append(MessageRecord.guild_id != guild_id)
+    if exclude_channel_ids:
+        for channel_id in exclude_channel_ids:
+            whereclause.append(MessageRecord.channel_id != channel_id)
+    if types:
+        for type in types:
+            whereclause.append(MessageRecord.type == type)
+    if detail_types:
+        for detail_type in detail_types:
+            whereclause.append(MessageRecord.detail_type == detail_type)
     if time_start:
         whereclause.append(MessageRecord.time >= time_start)
     if time_stop:
@@ -93,8 +100,54 @@ async def get_message_records(
     statement = select(MessageRecord).where(*whereclause)
     async with create_session() as session:
         records: List[MessageRecord] = (await session.exec(statement)).all()  # type: ignore
+    return records
 
+
+@overload
+async def get_messages(
+    bot: V11Bot, *, plain_text: Literal[False], **kwargs
+) -> List[V11Msg]:
+    ...
+
+
+@overload
+async def get_messages(
+    bot: V12Bot, *, plain_text: Literal[False], **kwargs
+) -> List[V12Msg]:
+    ...
+
+
+@overload
+async def get_messages(
+    bot: Union[V11Bot, V12Bot], *, plain_text: Literal[True] = ..., **kwargs
+) -> List[str]:
+    ...
+
+
+async def get_messages(
+    bot: Union[V11Bot, V12Bot], *, plain_text: bool = False, **kwargs
+) -> Union[List[str], List[V11Msg], List[V12Msg]]:
+    """
+    :说明:
+
+      获取消息记录的消息列表
+
+    :参数:
+
+      * ``bot: Union[V11Bot, V12Bot]``: Nonebot `Bot` 对象
+      * ``plain_text: bool = False``: 为 `True` 则返回字符串数组，否则返回 `Message` 数组
+      * ``**kwargs``: 其他参数，具体查看 `get_message_records` 中的定义
+
+    :返回值:
+
+      * ``Union[List[str], List[V11Msg], List[V12Msg]]``: 消息列表
+    """
+
+    records: List[MessageRecord] = await get_message_records(bot, **kwargs)
     if plain_text:
         return [record.alt_message for record in records]
     else:
-        return [deserialize_message(record.message) for record in records]
+        if isinstance(bot, V11Bot):
+            return [deserialize_message(record.message, V11Msg) for record in records]
+        else:
+            return [deserialize_message(record.message, V12Msg) for record in records]
