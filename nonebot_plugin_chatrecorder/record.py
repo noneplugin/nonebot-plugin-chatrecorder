a36b81bc396ea68e55493de5103bf77b5ab41d07
meetwq <meetwq@gmail.com>
2023-07-23 16:28:15 +0800

    修改接口

--- nonebot_plugin_chatrecorder/record.py
+++ nonebot_plugin_chatrecorder/record.py
@@ -1,9 +1,12 @@
 from datetime import datetime, timezone
-from typing import Iterable, List, Literal, Optional, Sequence
+from typing import Iterable, List, Literal, Optional, Sequence, Union
 
 from nonebot.adapters import Bot, Message
 from nonebot_plugin_datastore import create_session
+from nonebot_plugin_session import SessionLevel
+from nonebot_plugin_session.model import SessionModel
 from sqlalchemy import or_, select
+from sqlalchemy.orm import selectinload
 
 from .message import deserialize_message
 from .model import MessageRecord
@@ -20,40 +23,36 @@ def remove_timezone(dt: datetime) -> datetime:
 
 async def get_message_records(
     *,
-    bot_types: Optional[Iterable[str]] = None,
     bot_ids: Optional[Iterable[str]] = None,
+    bot_types: Optional[Iterable[str]] = None,
     platforms: Optional[Iterable[str]] = None,
+    levels: Optional[Iterable[Union[str, SessionLevel]]] = None,
+    id1s: Optional[Iterable[str]] = None,
+    id2s: Optional[Iterable[str]] = None,
+    id3s: Optional[Iterable[str]] = None,
+    exclude_id1s: Optional[Iterable[str]] = None,
+    exclude_id2s: Optional[Iterable[str]] = None,
+    exclude_id3s: Optional[Iterable[str]] = None,
     time_start: Optional[datetime] = None,
     time_stop: Optional[datetime] = None,
     types: Optional[Iterable[Literal["message", "message_sent"]]] = None,
-    detail_types: Optional[Iterable[str]] = None,
-    user_ids: Optional[Iterable[str]] = None,
-    group_ids: Optional[Iterable[str]] = None,
-    guild_ids: Optional[Iterable[str]] = None,
-    channel_ids: Optional[Iterable[str]] = None,
-    exclude_user_ids: Optional[Iterable[str]] = None,
-    exclude_group_ids: Optional[Iterable[str]] = None,
-    exclude_guild_ids: Optional[Iterable[str]] = None,
-    exclude_channel_ids: Optional[Iterable[str]] = None,
 ) -> Sequence[MessageRecord]:
     """获取消息记录
 
     参数:
-      * ``bot_types: Optional[Iterable[str]]``: 协议适配器类型列表，为空表示所有适配器
       * ``bot_ids: Optional[Iterable[str]]``: bot id 列表，为空表示所有 bot id
+      * ``bot_types: Optional[Iterable[str]]``: 协议适配器类型列表，为空表示所有适配器
       * ``platforms: Optional[Iterable[str]]``: 平台类型列表，为空表示所有平台
+      * ``levels: Optional[Iterable[Union[str, SessionLevel]]]``: 会话级别列表，为空表示所有级别
+      * ``id1s: Optional[Iterable[str]]``: 会话 id1（用户级 id）列表，为空表示所有 id
+      * ``id2s: Optional[Iterable[str]]``: 会话 id2（群组级 id）列表，为空表示所有 id
+      * ``id3s: Optional[Iterable[str]]``: 会话 id3（两级群组级 id）列表，为空表示所有 id
+      * ``exclude_id1s: Optional[Iterable[str]]``: 不包含的会话 id1（用户级 id）列表，为空表示不限制
+      * ``exclude_id2s: Optional[Iterable[str]]``: 不包含的会话 id2（群组级 id）列表，为空表示不限制
+      * ``exclude_id3s: Optional[Iterable[str]]``: 不包含的会话 id3（两级群组级 id）列表，为空表示不限制
       * ``time_start: Optional[datetime]``: 起始时间，UTC 时间，为空表示不限制起始时间
       * ``time_stop: Optional[datetime]``: 结束时间，UTC 时间，为空表示不限制结束时间
       * ``types: Optional[Iterable[Literal["message", "message_sent"]]]``: 消息事件类型列表，为空表示所有类型
-      * ``detail_types: Optional[List[str]]``: 消息事件具体类型列表，为空表示所有类型
-      * ``user_ids: Optional[Iterable[str]]``: 用户列表，为空表示所有用户
-      * ``group_ids: Optional[Iterable[str]]``: 群组列表，为空表示所有群组
-      * ``guild_ids: Optional[Iterable[str]]``: 两级群组消息群组列表，为空表示所有群组
-      * ``channel_ids: Optional[Iterable[str]]``: 两级群组消息频道列表，为空表示所有频道
-      * ``exclude_user_ids: Optional[Iterable[str]]``: 不包含的用户列表，为空表示不限制
-      * ``exclude_group_ids: Optional[Iterable[str]]``: 不包含的群组列表，为空表示不限制
-      * ``exclude_guild_ids: Optional[Iterable[str]]``: 不包含的两级群组消息群组列表，为空表示不限制
-      * ``exclude_channel_ids: Optional[Iterable[str]]``: 不包含的两级群组消息频道列表，为空表示不限制
 
     返回值:
       * ``List[MessageRecord]``: 消息记录列表
@@ -63,61 +62,46 @@ async def get_message_records(
 
     if bot_types:
         whereclause.append(
-            or_(*[MessageRecord.bot_type == bot_type for bot_type in bot_types])
+            or_(*[SessionModel.bot_type == bot_type for bot_type in bot_types])
         )
     if bot_ids:
-        whereclause.append(or_(*[MessageRecord.bot_id == bot_id for bot_id in bot_ids]))
+        whereclause.append(or_(*[SessionModel.bot_id == bot_id for bot_id in bot_ids]))
     if platforms:
         whereclause.append(
-            or_(*[MessageRecord.platform == platform for platform in platforms])
+            or_(*[SessionModel.platform == platform for platform in platforms])
         )
+    if levels:
+        whereclause.append(or_(*[SessionModel.level == level for level in levels]))
+    if id1s:
+        whereclause.append(or_(*[SessionModel.id1 == id1 for id1 in id1s]))
+    if id2s:
+        whereclause.append(or_(*[SessionModel.id2 == id2 for id2 in id2s]))
+    if id3s:
+        whereclause.append(or_(*[SessionModel.id3 == id3 for id3 in id3s]))
+    if exclude_id1s:
+        for id1 in exclude_id1s:
+            whereclause.append(SessionModel.id1 != id1)
+    if exclude_id2s:
+        for id2 in exclude_id2s:
+            whereclause.append(SessionModel.id2 != id2)
+    if exclude_id3s:
+        for id3 in exclude_id3s:
+            whereclause.append(SessionModel.id3 != id3)
     if time_start:
         whereclause.append(MessageRecord.time >= remove_timezone(time_start))
     if time_stop:
         whereclause.append(MessageRecord.time <= remove_timezone(time_stop))
     if types:
         whereclause.append(or_(*[MessageRecord.type == type for type in types]))
-    if detail_types:
-        whereclause.append(
-            or_(
-                *[
-                    MessageRecord.detail_type == detail_type
-                    for detail_type in detail_types
-                ]
-            )
-        )
-    if user_ids:
-        whereclause.append(
-            or_(*[MessageRecord.user_id == user_id for user_id in user_ids])
-        )
-    if group_ids:
-        whereclause.append(
-            or_(*[MessageRecord.group_id == group_id for group_id in group_ids])
-        )
-    if guild_ids:
-        whereclause.append(
-            or_(*[MessageRecord.guild_id == guild_id for guild_id in guild_ids])
-        )
-    if channel_ids:
-        whereclause.append(
-            or_(*[MessageRecord.channel_id == channel_id for channel_id in channel_ids])
-        )
-    if exclude_user_ids:
-        for user_id in exclude_user_ids:
-            whereclause.append(MessageRecord.user_id != user_id)
-    if exclude_group_ids:
-        for group_id in exclude_group_ids:
-            whereclause.append(MessageRecord.group_id != group_id)
-    if exclude_guild_ids:
-        for guild_id in exclude_guild_ids:
-            whereclause.append(MessageRecord.guild_id != guild_id)
-    if exclude_channel_ids:
-        for channel_id in exclude_channel_ids:
-            whereclause.append(MessageRecord.channel_id != channel_id)
 
-    statement = select(MessageRecord).where(*whereclause)
-    async with create_session() as session:
-        records = (await session.scalars(statement)).all()
+    statement = (
+        select(MessageRecord)
+        .where(*whereclause)
+        .join(SessionModel)
+        .options(selectinload(MessageRecord.session))
+    )
+    async with create_session() as db_session:
+        records = (await db_session.scalars(statement)).all()
     return records
 
 
@@ -131,8 +115,8 @@ async def get_messages(bot: Bot, **kwargs) -> List[Message]:
     返回值:
       * ``List[Message]``: 消息列表
     """
-    adapter_name = bot.adapter.get_name()
-    kwargs.update({"bot_types": [adapter_name]})
+    bot_type = bot.type
+    kwargs.update({"bot_types": [bot_type]})
     records = await get_message_records(**kwargs)
     return [deserialize_message(bot, record.message) for record in records]
 
