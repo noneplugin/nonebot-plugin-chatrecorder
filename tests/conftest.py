50cfdab4e99a991257a5c1f96e4f9bf6475bbb60
meetwq <meetwq@gmail.com>
2024-11-28 23:02:33 +0800

    fix

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,7 +8,6 @@ from nonebot.adapters.kaiheila import Adapter as KaiheilaAdapter
 from nonebot.adapters.onebot.v11 import Adapter as OnebotV11Adapter
 from nonebot.adapters.onebot.v12 import Adapter as OnebotV12Adapter
 from nonebot.adapters.qq import Adapter as QQAdapter
-from nonebot.adapters.red import Adapter as RedAdapter
 from nonebot.adapters.satori import Adapter as SatoriAdapter
 from nonebot.adapters.telegram import Adapter as TelegramAdapter
 from nonebug import NONEBOT_INIT_KWARGS, App
@@ -53,6 +52,5 @@ def load_adapters(nonebug_init: None):
     driver.register_adapter(OnebotV11Adapter)
     driver.register_adapter(OnebotV12Adapter)
     driver.register_adapter(QQAdapter)
-    driver.register_adapter(RedAdapter)
     driver.register_adapter(SatoriAdapter)
     driver.register_adapter(TelegramAdapter)
